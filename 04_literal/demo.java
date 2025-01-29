29aeecc663b2dfa7e54bbbeca554f39113b1175a
Chirag-04 <04chirag2001@gmail.com>
2025-01-29 17:29:15 +0530

    completed literals

---
+++ 04_literal/demo.java
@@ -0,0 +1,18 @@
+
+public class demo {
+    public static void main(String[] args) {
+        int num = 10;
+        int binary=0b1010;
+        int hexa = 0x4E;
+        int n = 100_00_0;
+        float avg = 123.455f;
+        char ch =  'a';
+        ch++;
+        System.out.println(ch); // b
+        System.out.println(num); // 10 
+        System.out.println(binary); // 10
+        System.out.println(hexa);
+        System.out.println(n);
+        
+    }
+}
