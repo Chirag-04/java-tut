597e39de4032ff810ac678cdbf86a2c554635e53
Chirag-04 <04chirag2001@gmail.com>
2025-01-29 13:16:48 +0530

    revised variables

---
+++ 02_variables/demo.java
@@ -0,0 +1,9 @@
+
+public class demo {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int sum =  x + y;
+        System.out.println(sum);
+    }    
+}
