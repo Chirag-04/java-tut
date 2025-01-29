36258b36c7b4524ce6902d04adb9d25f330d6c5b
Chirag-04 <04chirag2001@gmail.com>
2025-01-29 17:11:04 +0530

    covered datatypes

---
+++ 03_datatypes/demo.java
@@ -0,0 +1,16 @@
+public class demo {
+    public static void main(String[] args) {
+        int age = 12;
+        char ch = 'a';
+        double avg =  13.567;
+        float f =  12.45f;
+        long l = 123567l;
+        boolean b = true;
+        System.out.println(age);  
+        System.out.println(avg); 
+        System.out.println(f); 
+        System.out.println(l); 
+        System.out.println(b); 
+        
+    }
+}
