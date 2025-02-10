35f171ae9a995536986bcdc2220dd5730630f24c
Chirag-04 <04chirag2001@gmail.com>
2025-02-10 13:20:47 +0530

    functonal interface basic

---
+++ 24_java8/functionInterafacebasic.java
@@ -0,0 +1,14 @@
+
+
+// Function interface -> exactly one abstract methods ( can contain static and deafult method )
+@FunctionalInterface
+interface myInterface{
+    public void sayHi();
+    public void sayBye(); // multiple abstract methods are not allowed
+}
+
+public class functionInterafacebasic {
+    public static void main(String[] args) {
+        
+    }
+}
