80854b337f4293e623e5b6fa45454f7b0000e8cb
Chirag-04 <04chirag2001@gmail.com>
2025-02-10 13:09:33 +0530

    basics of lamda expression

---
+++ 24_java8/Lamda.java
@@ -0,0 +1,15 @@
+
+
+public class Lamda {
+    public static void main(String[] args){
+        System.out.println("hi");
+    }
+}
+
+
+
+private int getString(String str){
+    return str.length();
+}
+// converted to
+str-> str.length();
